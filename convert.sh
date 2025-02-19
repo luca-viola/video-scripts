01470fce5268a6a388f97e9ae343e37babf7f08b
Luca Viola <luca.viola@gmail.com>
2025-02-19 17:48:26 +0000

    added framerate, hardened convert script

--- convert.sh
+++ convert.sh
@@ -4,6 +4,7 @@ ABR=128
 W=640
 H=360
 SIZE=80
+FRAMERATE=30
 
 HAS_W=0
 HAS_H=0
@@ -16,41 +17,44 @@ else
   THREADS=$(grep -c ^processor /proc/cpuinfo)
 fi
 
-
 function usage()
 {
-  echo "Usage:" 
+  echo "Usage:"
   echo "  convert -i FILENAME"
   echo "          [-s SIZE] (in mega bytes, ex. -s 80)"
   echo "          [-a AUDIOBITRATE] (in k, es. -a 128)"
   echo "          [-w WIDTH [-h HEIGHT]]"
+  echo "          [-r framerate]"
   echo "          [-o OUTFILE]"
 }
 
-while getopts ":a:w:h:s:i:o:t:" opt; do
+while getopts ":a:w:h:s:i:o:r:t:" opt; do
   case $opt in
     a)
-      ABR=$OPTARG
+      ABR="$OPTARG"
       ;;
     w)
-      W=$OPTARG
+      W="$OPTARG"
       HAS_W=1
       ;;
     h)
-      H=$OPTARG
+      H="$OPTARG"
       HAS_H=1
       ;;
     s)
-      SIZE=$OPTARG
+      SIZE="$OPTARG"
       ;;
     i)
-      FILE=$OPTARG
+      FILE="$OPTARG"
       ;;
     o)
-      OUTFILE=$OPTARG
+      OUTFILE="$OPTARG"
+      ;;
+    r)
+      FRAMERATE="$OPTARG"
       ;;
     t)
-      THREADS=$OPTARG
+      THREADS="$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -69,7 +73,7 @@ done
 
 if [ -z "$FILE" ]; then
   usage
-  exit 
+  exit
 fi
 
 let "WH=$HAS_W ^ $HAS_H"
@@ -80,16 +84,16 @@ if [ $WH == 1 ]; then
 fi
 
 if [ -z "$OUTFILE" ]; then
-  EXT=`echo $FILE | awk -F . '{print $NF}'`
-  BASENAME=`basename $FILE .$EXT`
+  EXT=`echo "$FILE" | awk -F . '{print $NF}'`
+  BASENAME=`basename "$FILE" .$EXT`
   OUTFILE="$BASENAME-out-`date +"%Y%m%d%H%M%S"`.mp4"
 else
-  EXT=`echo $OUTFILE | awk -F . '{print $NF}'`
-  BASENAME=`basename $OUTFILE .$EXT`
+  EXT=`echo "$OUTFILE" | awk -F . '{print $NF}'`
+  BASENAME=`basename "$OUTFILE" .$EXT`
   OUTFILE="$BASENAME"
 fi
 
-DURATION=$(ffprobe -loglevel error -show_format -show_streams $FILE -print_format flat | grep "format.duration=" | cut -d "\"" -f 2)
+DURATION=$(ffprobe -loglevel error -show_format -show_streams "$FILE" -print_format flat | grep "format.duration=" | cut -d "\"" -f 2)
 
 VBR=$(echo "(($SIZE*8192)/$DURATION)-$ABR" | bc)
 let "BUFSIZE=(VBR*2)"
@@ -117,11 +121,12 @@ else
 fi
 
 echo "Starting conversion"
-echo "Input:           $FILE"
-echo "Output:          $OUTFILE"
+echo "Input:           \"$FILE\""
+echo "Output:          \"$OUTFILE\""
 echo "Video width :    $W"
 echo "Video height:    $H"
 echo "Video duration:  $DURATION sec"
+echo "Framerate:       $FRAMERATE"
 echo "Final size:      $SIZE mb"
 echo "Audio bitrate:   $ABR kb/s"
 echo "Video bitrate:   $VBR kb/s"
@@ -135,16 +140,16 @@ echo "*******************************************"
 echo "*********** GENERATING MP4 FILE ***********"
 echo "*******************************************"
 
-ffmpeg -y -i $FILE -c:v ${codec} -profile:v baseline -b:v "$VBR"k -pass 1 -an -s "$W"x"$H" -pix_fmt yuv420p -threads $THREADS -f mp4 /dev/null
- 
-ffmpeg -y -i $FILE -c:v ${codec} -profile:v baseline -b:v "$VBR"k -s "$W"x"$H" -pass 2 -pix_fmt yuv420p -strict -2 -c:a aac -b:a "$ABR"k -movflags faststart -threads $THREADS "$OUTFILE".mp4
+ffmpeg -y -i "$FILE" -c:v ${codec} -profile:v baseline -b:v "$VBR"k -pass 1 -an -s "$W"x"$H" -r $FRAMERATE -pix_fmt yuv420p -threads $THREADS -f mp4 /dev/null
+
+ffmpeg -y -i "$FILE" -c:v ${codec} -profile:v baseline -b:v "$VBR"k -s "$W"x"$H" -r $FRAMERATE -pass 2 -pix_fmt yuv420p -strict -2 -c:a aac -b:a "$ABR"k -movflags faststart -threads $THREADS "$OUTFILE".mp4
 
 if [ "$webm" == "1" ]; then
   echo "********************************************"
   echo "*********** GENERATING WEBM FILE ***********"
   echo "********************************************"
 
-  ffmpeg -i $FILE  -codec:v libvpx -quality good -cpu-used 0 -b:v "$VBR"k -qmin 10 -qmax 42 -maxrate "$VBR"k -bufsize "$BUFSIZE"k -vf scale=$W:$H -an -pass 1 -threads $THREADS -f webm /dev/null
+  ffmpeg -i "$FILE" -codec:v libvpx -quality good -cpu-used 0 -b:v "$VBR"k -qmin 10 -qmax 42 -maxrate "$VBR"k -bufsize "$BUFSIZE"k -vf scale=$W:$H -r $FRAMERATE -an -pass 1 -threads $THREADS -f webm /dev/null
 
-  ffmpeg -i $FILE -codec:v libvpx -quality good -cpu-used 0 -b:v "$VBR"k -qmin 10 -qmax 42 -maxrate "$WEBMAXSIZEPASS2"k -bufsize "$WEBBUFSIZEPASS2"k -threads $THREADS -vf scale=$W:$H -codec:a libvorbis -b:a "$ABR"k -pass 2 -f webm "$OUTFILE".webm
+  ffmpeg -i "$FILE" -codec:v libvpx -quality good -cpu-used 0 -b:v "$VBR"k -qmin 10 -qmax 42 -maxrate "$WEBMAXSIZEPASS2"k -bufsize "$WEBBUFSIZEPASS2"k -threads $THREADS -vf scale=$W:$H -r $FRAMERATE -codec:a libvorbis -b:a "$ABR"k -pass 2 -f webm "$OUTFILE".webm
 fi
